78517a181a75b10494b4f7183acdb3212e6865be
Reptile <reptitle@outlook.com>
2020-12-26 11:54:47 -0500

    Adding our Serverless API

---
+++ libs/handler-lib.ts
@@ -0,0 +1,26 @@
+import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+
+
+export default function handler(lambda) {
+  return async function(
+    event: APIGatewayEvent,
+    context: Context
+  ): Promise<APIGatewayProxyResult> {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
